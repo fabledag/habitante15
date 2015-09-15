83efc34ef8d5e7a183a6790ad7563a22f243cf14
fab <fabiolal@victoria.sh>
2015-09-15 16:48:37 -0500

    cambios widgets

--- js/main.js
+++ js/main.js
@@ -10,43 +10,43 @@ main.sliderHome = function(){
     });
 };
 
-main.drawCharts = function(){
-    $(".pieChart").drawPieChart([
-        { title: "Facebook",   value : 20,  color: "#76ccce" },
-        { title: "Twitter",    value:  20,  color: "#00acad" },
-        { title: "Google",     value:  60,  color: "#333332" }
-    ]);
-    $(".pieChart2").drawPieChart([
-        { title: "Facebook",   value : 20,  color: "#76ccce" },
-        { title: "Twitter",    value:  20,  color: "#00acad" },
-        { title: "Google",     value:  60,  color: "#333332" }
-    ]);
-    $(".pieChart3").drawPieChart([
-        { title: "Facebook",   value : 10,  color: "#76ccce" },
-        { title: "Twitter",    value:  70,  color: "#00acad" },
-        { title: "Google",     value:  20,  color: "#333332" }
-    ]);
-
-
-
-    $(".pieChart4").drawPieChart([
-        { title: "Facebook",   value : 20,  color: "#76ccce" },
-        { title: "Twitter",    value:  20,  color: "#00acad" },
-        { title: "Google",     value:  60,  color: "#333332" }
-    ]);
-    $(".pieChart5").drawPieChart([
-        { title: "Facebook",   value : 20,  color: "#76ccce" },
-        { title: "Twitter",    value:  20,  color: "#00acad" },
-        { title: "Google",     value:  60,  color: "#333332" }
-    ]);
-    $(".pieChart6").drawPieChart([
-        { title: "Facebook",   value : 10,  color: "#76ccce" },
-        { title: "Twitter",    value:  70,  color: "#00acad" },
-        { title: "Google",     value:  20,  color: "#333332" }
-    ]);
-};
+//main.drawCharts = function(){
+//    $(".pieChart").drawPieChart([
+//        { title: "Facebook",   value : 20,  color: "#76ccce" },
+//        { title: "Twitter",    value:  20,  color: "#00acad" },
+//        { title: "Google",     value:  60,  color: "#333332" }
+//    ]);
+//    $(".pieChart2").drawPieChart([
+//        { title: "Facebook",   value : 20,  color: "#76ccce" },
+//        { title: "Twitter",    value:  20,  color: "#00acad" },
+//        { title: "Google",     value:  60,  color: "#333332" }
+//    ]);
+//    $(".pieChart3").drawPieChart([
+//        { title: "Facebook",   value : 10,  color: "#76ccce" },
+//        { title: "Twitter",    value:  70,  color: "#00acad" },
+//        { title: "Google",     value:  20,  color: "#333332" }
+//    ]);
+//
+//
+//
+//    $(".pieChart4").drawPieChart([
+//        { title: "Facebook",   value : 20,  color: "#76ccce" },
+//        { title: "Twitter",    value:  20,  color: "#00acad" },
+//        { title: "Google",     value:  60,  color: "#333332" }
+//    ]);
+//    $(".pieChart5").drawPieChart([
+//        { title: "Facebook",   value : 20,  color: "#76ccce" },
+//        { title: "Twitter",    value:  20,  color: "#00acad" },
+//        { title: "Google",     value:  60,  color: "#333332" }
+//    ]);
+//    $(".pieChart6").drawPieChart([
+//        { title: "Facebook",   value : 10,  color: "#76ccce" },
+//        { title: "Twitter",    value:  70,  color: "#00acad" },
+//        { title: "Google",     value:  20,  color: "#333332" }
+//    ]);
+//};
 
 $(function(){
-    main.drawCharts();
+    //main.drawCharts();
     main.sliderHome();
 });
